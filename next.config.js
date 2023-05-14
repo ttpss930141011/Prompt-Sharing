183ca672249ccbf2a40b1df1dfc332239629d52c
Justin Xiao <o927416847@gmail.com>
2023-05-15 00:01:28 +0800

    fix: fixed Avatar Unhandled Runtime Error
    fixed #3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] },
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+    },
     webpack(config) {
         config.experiments = { ...config.experiments, topLevelAwait: true };
         return config;
