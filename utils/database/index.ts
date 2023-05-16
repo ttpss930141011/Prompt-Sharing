6ecc0ed0ba632a1f831c27b13781737823eed17d
Justin Xiao <o927416847@gmail.com>
2023-05-16 17:27:43 +0800

    refactor: removed unessarary event listener & replaced isConnect to mongoose.connection.readyState

--- utils/database/index.ts
+++ utils/database/index.ts
@@ -1,8 +1,6 @@
 import { db } from "@/config";
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 export const connectToDB = async () => {
     mongoose.set("strictQuery", false);
 
@@ -16,39 +14,27 @@ export const connectToDB = async () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         maxPoolSize: 10, // Maintain up to 10 socket connections
-        // connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-        // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     };
-
-    mongoose.connection.once("connected", () => {
+    if (mongoose.connection.readyState === 1) {
+        console.log("MongoDB is already connected");
+        return;
+    }
+    try {
+        await mongoose.connect(dbURI, options);
         console.info("Mongoose default connection open to " + dbURI);
-    });
-
-    // If the connection throws an error
-    mongoose.connection.on("error", (err) => {
+    } catch (err) {
         console.error("Mongoose default connection error: " + err);
-    });
+    }
 
     // When the connection is disconnected
     mongoose.connection.on("disconnected", () => {
-        isConnected = false;
         console.info("Mongoose default connection disconnected");
     });
 
     // // If the Node process ends, close the Mongoose connection
     process.on("SIGINT", async () => {
         await mongoose.connection.close();
-        isConnected = false;
         console.info("Mongoose default connection disconnected through app termination");
         process.exit(0);
     });
-
-    try {
-        await mongoose.connect(dbURI, options);
-        console.info("Mongoose connection done");
-        isConnected = true;
-    } catch (err) {
-        console.info("Mongoose connection error");
-        console.error(err);
-    }
 };
